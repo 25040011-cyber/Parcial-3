8a5f4165e18dd0612940b33882996a3451c319b3
25040011-cyber <25040011@utculiacan.edu.mx>
2026-08-20 12:23:18 -0600

    Create 06mensajes.py

---
+++ mensajes.py
@@ -0,0 +1,12 @@
+def bienvenida():
+    print("\n==============================")
+    print("     ORGANIZADOR DE PELICULAS")
+    print("==============================")
+
+
+def despedida():
+    print("\nGracias por usar el programa")
+
+
+def no_encontrada():
+    print("La pelicula no se encuentra en la lista")
